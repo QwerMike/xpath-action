ccc5eb56d1379180db02d0ba313109f46e3b6744
Mykhailo Kaskun <tmikent@gmail.com>
2020-03-31 21:49:31 +0300

    Implement xpath action

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const xpath = require('xpath');
+const DOMParser = require('xmldom').DOMParser;
+const fs = require('fs');
+
+try {
+    const filename = core.getInput('filename');
+    const expression = core.getInput('expression');
+    const namespaces = core.getInput('namespaces');
+
+    const content = fs.readFileSync(filename, 'utf8');
+    const document = new DOMParser().parseFromString(content);
+    const select = namespaces
+        ? xpath.useNamespaces(JSON.parse(namespaces)) 
+        : xpath.select;
+    const nodes = select(expression, document);
+
+    const result = nodes.map(node => node.toString()).join("\n");
+    core.setOutput("result", result);
+
+} catch (error) {
+    core.setFailed(error.message);
+}
