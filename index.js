2e6447e0d31801683037819249f9f3a292023762
John Molakvoæ <skjnldsv@users.noreply.github.com>
2021-10-29 10:40:32 +0200

    Fix output formatting

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const core = require('@actions/core');
-const xpath = require('xpath');
-const DOMParser = require('xmldom').DOMParser;
-const fs = require('fs');
+const core = require("@actions/core");
+const xpath = require("xpath");
+const DOMParser = require("xmldom").DOMParser;
+const fs = require("fs");
 
 try {
-    const filename = core.getInput('filename');
-    const expression = core.getInput('expression');
-    const namespaces = core.getInput('namespaces');
+    const filename = core.getInput("filename");
+    const expression = core.getInput("expression");
+    const namespaces = core.getInput("namespaces");
 
-    const content = fs.readFileSync(filename, 'utf8');
+    const content = fs.readFileSync(filename, "utf8");
     const document = new DOMParser().parseFromString(content);
     const select = namespaces
-        ? xpath.useNamespaces(JSON.parse(namespaces)) 
+        ? xpath.useNamespaces(JSON.parse(namespaces))
         : xpath.select;
+ 
+    const result = {};
     const nodes = select(expression, document);
 
-    const result = nodes.map(node => node.toString()).join("\n");
-    core.setOutput("result", result);
+    nodes.forEach((node) => {
+        // If attribute, map values
+        if (node.name) {
+            result[node.name] = node.value;
+            return;
+        }
+        result[node.tagName] = node.toString();
+    });
 
+    core.setOutput("result", JSON.stringify(result));
 } catch (error) {
     core.setFailed(error.message);
 }
