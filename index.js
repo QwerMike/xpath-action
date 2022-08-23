2400fc3fd167687ad3b4f96793da0e2b587bbee0
Levi Bostian <levi@levi.earth>
2022-08-23 12:06:18 -0500

    feat: allow passing in string instead of file (#1)

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const DOMParser = require('xmldom').DOMParser;
 const fs = require('fs');
 
 try {
-    const filename = core.getInput('filename');
+    const documentString = core.getInput('documentString');
+    const filename = core.getInput('filename');    
     const expression = core.getInput('expression');
     const namespaces = core.getInput('namespaces');
-
-    const content = fs.readFileSync(filename, 'utf8');
+    
+    const content = documentString || fs.readFileSync(filename, 'utf8');
     const document = new DOMParser().parseFromString(content);
     const select = namespaces
         ? xpath.useNamespaces(JSON.parse(namespaces)) 
